cefce2cabe0f7793ad18cb6bbd98ec84633589cf
navilan <navilan@folds.in>
2022-10-10 03:21:30 +0530

    fix update frequency and icons

--- darwin/sketchybar/config/plugins/music.sh
+++ darwin/sketchybar/config/plugins/music.sh
@@ -4,14 +4,13 @@ source "$HOME/.config/sketchybar/colors.sh" # Loads all defined colors
 
 STS=$(spt pb --status 2>&1)
 ICON=$(echo     "$STS" | awk '{ print $2 }')
-SHUFFLE=$(echo  "$STS" | awk '{ print $1 }')
 
 STS_PLAYING="▶"
 STS_PAUSED="⏸"
 
 SONG=$(echo     "$STS" | awk -F "$STS_PLAYING | $STS_PAUSED" '{ print $2 }')
-PU="⏸"
-PL="▶"
+PU="􀊆"
+PL="􀊄"
 ST="■"
 LBL=$(echo "$SONG" | cut -c 1-50)
 
